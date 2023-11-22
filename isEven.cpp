fbc99fc9b88d8f489e763d467fe4cc15f2cd59e2
Justin <menziesjustin@ymail.com>
2023-11-22 18:58:53 -0500

    - fixed whitespace, and added comments

--- isEven.cpp
+++ isEven.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter a number";
+    cout << "Enter a number: ";
     int num{};
     cin >> num;
-
+    // checks if the number is even and returns even or odd
     cout << num << " is " << ((num % 2 == 0) ? "even" : "odd");
 }

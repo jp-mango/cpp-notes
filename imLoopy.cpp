1619982cda5ad0dcaf3fd3c47e609e825829ecd3
Justin <menziesjustin@ymail.com>
2023-11-22 19:30:20 -0500

    - added for loop example to imLoopy

--- imLoopy.cpp
+++ imLoopy.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
     // For Loops - iterates a specific number of times
+    cout << "-- For Loops -- " << endl;
+    cout << "(int i{1} ; i <= 5 ; i++)" << endl;
+    for (int i{1}; i <= 5; i++)
+        cout << i << endl;
+    cout << endl;
 
     // Ranged-based For Loops - one iteration for each element in a range or collection
 

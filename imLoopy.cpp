d944896dd6417416d9f651005f44182b60e60641
Justin <menziesjustin@ymail.com>
2023-11-22 19:21:13 -0500

    - added notes on loops

---
+++ imLoopy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // For Loops - iterates a specific number of times
+
+    // Ranged-based For Loops - one iteration for each element in a range or collection
+
+    // While Loops - iterates while a condition remains true, check condition at the BEGINNING the next iteration
+
+    // Do-While Loop - iterates while the condition remains true, checks condition at the END of every iteration
+}

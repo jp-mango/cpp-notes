4eaf1fb2df57e90337d4871a7390d5057d7bda8f
Justin <menziesjustin@ymail.com>
2023-11-25 17:10:02 -0500

    - finished loop notes w/ nested loop example

--- imLoopy.cpp
+++ imLoopy.cpp
@@ -116,4 +116,14 @@ int main()
             cout << val << endl;
         }
     }
+    // Nested Loops - a loop within a loop. The inner loop finished before the outer.
+    cout << "\n----- Nested Loops ----- " << endl;
+    for (int num1{1}; num1 <= 10; num1++)
+    {
+        for (int num2{1}; num2 <= 10; num2++)
+        {
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+        }
+        cout << "---------------" << endl;
+    }
 }

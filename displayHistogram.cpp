5886d1388c2c93e4eb2f1832d53026f8fb85feb1
Justin <menziesjustin@ymail.com>
2023-11-26 15:53:14 -0500

    - added a script that displays a histogram from a vector

---
+++ displayHistogram.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    //
+    int num_items{};
+    cout << "How many data items do you have? ";
+    cin >> num_items;
+
+    vector<int> data{};
+    for (int i{1}; i <= num_items; i++)
+    {
+        int data_item{};
+        cout << "Enter data item " << i << ": ";
+        cin >> data_item;
+        data.push_back(data_item);
+    }
+
+    cout << "\nDisplaying Histogram" << endl;
+    for (auto value : data)
+    {
+        for (int i{1}; i <= value; i++)
+            cout << ((i % 5 == 0) ? "*" : "-");
+        cout << endl;
+    }
+    return 0;
+}

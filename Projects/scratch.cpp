740c1ceefae21da2030f3939214c1d3aab96192b
Justin <menziesjustin@ymail.com>
2023-12-01 14:51:11 -0500

    - scratch

--- Projects/scratch.cpp
+++ Projects/scratch.cpp
@@ -1,12 +1,11 @@
-#include <cctype>
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
-  char letter{'h'};
-  char newLetter = letter + 1;
-  cout << letter << " " << newLetter;
+  int num{26};
+  cout << "Hello World but what happens when i have a really long string her "
+          "and it has to wrap"
+       << endl;
   return 0;
 }

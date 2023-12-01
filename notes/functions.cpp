a892617c2cdc2f0c2ca31b3f5a0fc25ce27be683
Justin <menziesjustin@ymail.com>
2023-12-01 14:51:03 -0500

    - added better notes to functions

--- notes/functions.cpp
+++ notes/functions.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 // Function Prototypes
-int add1(int a, int b = 10);
-int add2(int a, int b, int c);
+int add(int a, int b = 10);
+int add(int a, int b, int c);
 void printArray(int arr[], int size);
 void increment(int &num);
 int factorial(int n);
@@ -12,11 +12,11 @@ inline int max(int a, int b);
 int main() {
   // Using the add function with default argument
   // Only one argument is provided, the second uses the default value
-  cout << "Add with default argument: " << add1(5) << endl;
+  cout << "Add with default argument: " << add(5) << endl;
 
   // Overloading: Using add function with 3 parameters
   // This calls the second version of the add function
-  cout << "Overloaded add: " << add2(5, 10, 15) << endl;
+  cout << "Overloaded add: " << add(5, 10, 15) << endl;
 
   // Passing an array to a function
   int arr[] = {1, 2, 3, 4, 5};
@@ -42,22 +42,27 @@ int main() {
 
 // add function with default argument
 // If the second argument is not provided, it defaults to 10
-int add1(int a, int b) { return a + b; }
+int add(int a, int b) { return a + b; }
 
-// Overloaded add function with three parameters
-// Different number of parameters allows for function overloading
-int add2(int a, int b, int c) { return a + b + c; }
+// Overloaded add function with three parameters, this has the same name as the
+// above function, but since i passed in 3 variables the compiler knows which
+// function to pass it to Different number of parameters allows for function
+// overloading
+int add(int a, int b, int c) { return a + b + c; }
 
 // Function to print elements of an array
-// The array is passed as a pointer (int arr[]) and size is also passed
+// The array is passed as a pointer (int arr[]) and size is also passed because
+// only the memory address of the array is passed, so the compiler doesn't know
+// the size of the array. Therfore it has to be passed in as well.
 void printArray(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     cout << arr[i] << " ";
   }
 }
 
-// Pass-by-reference function
-// num is passed by reference, so the function can modify the original variable
+// Pass-by-reference function. Allows the function to modify the variable
+// directly. num is passed by reference, so the function can modify the original
+// variable
 void increment(int &num) { num++; }
 
 // Recursive function for calculating factorial
